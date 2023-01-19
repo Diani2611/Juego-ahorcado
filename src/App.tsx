e85ed4d8469bf584d2c906e2d86f7f55a2fa788a
Diani2611 <diana.261111@gmail.com>
2023-01-18 21:43:50 -0500

    Ajuste / número de intentos permitidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { letters } from './helpers/letters'
 import { HangImage } from './components/HangImage'
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
 
@@ -9,8 +9,26 @@ function App() {
   const [hiddenWord, setHiddenWord] = useState('_ '.repeat(word.length));
   const [attempts, setAttempts] = useState(0);
   const [lose, setLose] = useState ( false);
+  const [won, setWon] = useState (false);
+  
+
+  useEffect (( )=> {
+    if (attempts >= 9) {
+      setLose (true);
+    }
+  }, [attempts]);
+
+  useEffect(()=> {
+    const currentHiddenWord = hiddenWord.split('').join('');
+    if (currentHiddenWord === word) {
+      setWon (true);
+    }
+
+  }, [hiddenWord])
 
   const checkLetter = (letter: string) => {
+
+    if (lose) return;
 
     if (!word.includes(letter)) {
       setAttempts(Math.min(attempts + 1, 9));
@@ -39,7 +57,17 @@ function App() {
 
       <h3>{hiddenWord}</h3>
 
-      <h3>Intento: {attempts}  </h3>
+      <h3>Intentos: {attempts}  </h3>
+
+      {
+        (lose) 
+        ? <h2>Perdió { word }</h2> :''
+      }
+
+{
+        (won) 
+        ? <h2>Felicidades usted gan { word }</h2> :''
+      }
 
       {
         letters.map((letter) => (
